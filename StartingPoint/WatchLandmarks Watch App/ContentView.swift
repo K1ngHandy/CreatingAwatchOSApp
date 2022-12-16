e9e8532761463aa3a1c9b32601b0cf6f87fd079a
Steve H <a.king.life@pm.me>
2022-12-16 15:45:01 -0500

    shared files management

---
+++ StartingPoint/WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Stephen Handy on 12/16/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StartingPoint/WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Stephen Handy on 12/16/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
